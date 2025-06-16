42154889ae9bf6c126b40ed7c580450538e2cb5e
João Silva <buzelin2@gmail.com>
2025-06-15 22:53:13 -0300

    Commit 8 - classe repositorio

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 const { readFileSync } = require('fs');
 
+// Repositório de dados das peças
+class Repositorio {
+  constructor() {
+    this.pecas = JSON.parse(readFileSync('./pecas.json'));
+  }
+
+  getPeca(apre) {
+    return this.pecas[apre.id];
+  }
+}
+
 // formata centavos em "R$ x.xxx,xx"
 function formatarMoeda(valorCentavos) {
   return new Intl.NumberFormat("pt-BR", {
@@ -9,10 +20,14 @@ function formatarMoeda(valorCentavos) {
   }).format(valorCentavos / 100);
 }
 
-// classe de serviço com todos os cálculos
+// serviço de cálculo, agora usando o repositório
 class ServicoCalculoFatura {
-  calcularTotalApresentacao(pecas, apre) {
-    const peca = pecas[apre.id];
+  constructor(repo) {
+    this.repo = repo;
+  }
+
+  calcularTotalApresentacao(apre) {
+    const peca = this.repo.getPeca(apre);
     let total = 0;
     switch (peca.tipo) {
       case "tragedia":
@@ -34,56 +49,50 @@ class ServicoCalculoFatura {
     return total;
   }
 
-  calcularCredito(pecas, apre) {
+  calcularCredito(apre) {
     let creditos = 0;
     creditos += Math.max(apre.audiencia - 30, 0);
-    if (pecas[apre.id].tipo === "comedia") {
+    if (this.repo.getPeca(apre).tipo === "comedia") {
       creditos += Math.floor(apre.audiencia / 5);
     }
     return creditos;
   }
 
-  calcularTotalFatura(pecas, apresentacoes) {
+  calcularTotalFatura(apresentacoes) {
     return apresentacoes
-      .map(a => this.calcularTotalApresentacao(pecas, a))
+      .map(a => this.calcularTotalApresentacao(a))
       .reduce((acc, cur) => acc + cur, 0);
   }
 
-  calcularTotalCreditos(pecas, apresentacoes) {
+  calcularTotalCreditos(apresentacoes) {
     return apresentacoes
-      .map(a => this.calcularCredito(pecas, a))
+      .map(a => this.calcularCredito(a))
       .reduce((acc, cur) => acc + cur, 0);
   }
 }
 
-// função “apenas apresentação” agora recebe o serviço de cálculo
-function gerarFaturaStr(fatura, pecas, calc) {
+// apresentação em texto
+function gerarFaturaStr(fatura, calc) {
   let faturaStr = `Fatura ${fatura.cliente}\n`;
 
   for (let apre of fatura.apresentacoes) {
-    const peca  = pecas[apre.id];
-    const total = calc.calcularTotalApresentacao(pecas, apre);
+    const peca  = calc.repo.getPeca(apre);
+    const total = calc.calcularTotalApresentacao(apre);
     faturaStr += `  ${peca.nome}: ${formatarMoeda(total)} (${apre.audiencia} assentos)\n`;
   }
 
   faturaStr += `Valor total: ${formatarMoeda(
-    calc.calcularTotalFatura(pecas, fatura.apresentacoes)
+    calc.calcularTotalFatura(fatura.apresentacoes)
   )}\n`;
   faturaStr += `Créditos acumulados: ${calc.calcularTotalCreditos(
-    pecas,
     fatura.apresentacoes
   )} \n`;
 
   return faturaStr;
 }
 
-// geração em HTML comentada 
-// function gerarFaturaHTML
-
 // main
 const faturas = JSON.parse(readFileSync('./faturas.json'));
-const pecas    = JSON.parse(readFileSync('./pecas.json'));
-const calc     = new ServicoCalculoFatura();
+const calc    = new ServicoCalculoFatura(new Repositorio());
 
-console.log(gerarFaturaStr(faturas, pecas, calc));
-// console.log(gerarFaturaHTML(faturas, pecas));
+console.log(gerarFaturaStr(faturas, calc));
